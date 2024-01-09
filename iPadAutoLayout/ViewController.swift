cf542e8848d59523cd430915e555d906c593f425
MartinezSafari <martinez@thereachtrust.org>
2024-01-09 13:11:11 +0200

    Implement WebKit and WKNavigationDelegate to display the web page

--- iPadAutoLayout/ViewController.swift
+++ iPadAutoLayout/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet var webView : WKWebView!
+    @IBOutlet var activity : UIActivityIndicatorView!
+    
+    @IBOutlet var tfName : UITextField!
+    @IBOutlet var tfEmail : UITextField!
+    
+    @IBOutlet var lbName : UILabel!
+    @IBOutlet var lbEmail : UILabel!
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activity.isHidden = false
+        activity.startAnimating()
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activity.isHidden = true
+        activity.stopAnimating()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Connect to my GitHub repository
+        let urlAddress = URL(string: "https://www.nintendo.com/us/store/games/")
+        let url = URLRequest(url: urlAddress!)
+        webView.load(url)
+        webView.navigationDelegate = self
+        
     }
 
 
